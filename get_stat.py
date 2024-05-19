be351edcb92833943fc655add3feefafd2814edc
s1renaG <vaceslavmakarovic@gmail.com>
2024-05-19 18:30:43 -0400

    Update get_stat.py

--- get_stat.py
+++ get_stat.py
@@ -1,3 +1,22 @@
+import random
+
+class RandomGenerator:
+    def random_library(self):
+        randomizer = random.sample(range(0, 50), 30)
+        return randomizer
+
+    def stat_library(self) -> dict:
+        randomizer = self.random_library()
+        w = randomizer[0]
+        l = randomizer[1]
+        k = randomizer[2]
+        a = randomizer[3]
+        d = randomizer[4]
+        return {'w': w, 'l': l, 'k': k, 'a': a, 'd': d}
+
+random_gen = RandomGenerator()
+player_stats = f'{random_gen.stat_library()["k"]}-{random_gen.stat_library()["a"]}-{random_gen.stat_library()["d"]}'
+match_score = f'{random_gen.stat_library()["w"]}-{random_gen.stat_library()["l"]}'
 def stats(match_score:str, player_stats:str) -> dict:
   team1_score, team2_score = map(int, match_score.split('-'))
   kills, assists, deaths = map(int, player_stats.split('-'))
@@ -19,5 +38,4 @@ def stats(match_score:str, player_stats:str) -> dict:
     file.write(str(result))
   print(result)
   return
-
-print(stats("13-10", "25-0-5"))
+random_stats = stats(match_score, player_stats)
