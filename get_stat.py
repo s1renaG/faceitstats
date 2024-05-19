492aad9a5bc8d844653e865d560bb7b152acd590
s1renaG <vaceslavmakarovic@gmail.com>
2024-05-19 13:22:07 -0400

    Update get_stat.py

--- get_stat.py
+++ get_stat.py
@@ -1,12 +1,12 @@
 def stats(match_score:str, player_stats:str) -> dict:
   team1_score, team2_score = map(int, match_score.split('-'))
-  kills, deaths, assists = map(int, player_stats.split('-'))
+  kills, assists, deaths = map(int, player_stats.split('-'))
 
   total_rounds = team1_score + team2_score
 
   kd = kills / deaths if deaths != 0 else kills
   kr = kills / total_rounds
-  survival_rate = deaths/total_rounds
+  survival_rate = deaths / total_rounds if deaths != 0 else 0
 
   result = {
   'kd': kd,
@@ -17,7 +17,7 @@ def stats(match_score:str, player_stats:str) -> dict:
 
   with open("results.txt","w") as file:
     file.write(str(result))
+  print(result)
   return
 
 print(stats("13-10", "25-0-5"))
-
